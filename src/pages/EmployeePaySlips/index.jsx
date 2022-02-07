d62bd588ad8d84f099d0dcceb3baa08399e59fe4
imamnghr <imamn208@gmail.com>
2022-02-07 20:28:18 +0700

    side menu pages

---
+++ src/pages/EmployeePaySlips/index.jsx
@@ -0,0 +1,18 @@
+import SideMenu from "../../components/molecules/SideMenu";
+
+const EmployeePaySlips = () => {
+  return (
+    <div className="mt-3">
+      <div
+        className="mycontainer mobile:mycontainerfull py-7 gap-x-7 grid"
+        style={{ gridTemplateColumns: "1.2fr 5fr" }}
+      >
+        <div>
+          <SideMenu />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeePaySlips;
